f8ddd4a85ce4e4da471c51c3af62ad345bf2dc3d
IldarDavletyarov <ildardaf@gmail.com>
2026-01-05 20:37:31 +0500

    chore: image and api client

--- src/shared/api/image/getImageUrl.ts
+++ src/shared/api/image/getImageUrl.ts
@@ -1,4 +1,4 @@
-import { BASE_URL } from '../client'
+import { BASE_URL } from '../client/config'
 
 export function getImageUrl(posterPath: string): string {
   return posterPath?.startsWith('http') ? posterPath : `${BASE_URL}${posterPath}`
